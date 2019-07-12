e432b710e3ccd7a65dea7c6faf2633d5967966ab
siviwe <xboyfan01>
2019-07-11 23:47:04 -0700

    Connected to API

--- CityBoard/src/app/home/home.page.ts
+++ CityBoard/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {ChatService} from '../services/chat.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  posts: Object;
+  city='';
+
+  constructor(private chatService:ChatService) {}
+
+  ngOnInit(){
+    this.chatService.getPosts().subscribe(data =>{
+      this.posts = data;
+      
+    });
+  }
+
+  searchByCity(){
+    this.chatService.getCityPosts(this.city).subscribe(data=>{
+      this.posts=data;
+    });
+
+  }
 
 }
--- CityBoard/src/app/services/chat.service.ts
+++ CityBoard/src/app/services/chat.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+  url = 'http://polar-retreat-28833.herokuapp.com/api/chats/';
+
+  getPosts() : Observable<any>{
+    return this.http.get(this.url);
+  }
+
+  getCityPosts(city : String) {
+    return this.http.get(this.url+`/${city}`);
+  }
+
+  postMessage(city : String, content: String) {
+    return this.http.post(this.url,{city:city,content:content});
+  }
 }
